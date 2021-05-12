9e2b0773923121f3ca03785485e6126dc59b7dbc
thereciprocalsolutions <59396052+thereciprocalsolutions@users.noreply.github.com>
2021-05-12 23:14:16 +0530

    Update map.js

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 /* 
 copy past in using file
+onclick="calcRoute();"
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBNKpr4auBLgBgiOXJ41UUWTB58MZa6p3E&libraries=places"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://thereciprocalsolutions.github.io/WEPAPI/map.js"></script> 
