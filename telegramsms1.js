a5275ca3edd94a9fccced228322094cdc67d92f4
thereciprocalsolutions <59396052+thereciprocalsolutions@users.noreply.github.com>
2021-05-12 23:13:29 +0530

    Update telegramsms1.js

--- telegramsms1.js
+++ telegramsms1.js
@@ -1,4 +1,8 @@
-  
+ /* 
+copy past in using file
+onclick="sent();"
+<script src="https://thereciprocalsolutions.github.io/WEPAPI/localstoragesms.js"></script> 
+*/ 
 function sent() {
     var Name = document.getElementById("name").value;
     var Phone = document.getElementById("phone").value;
